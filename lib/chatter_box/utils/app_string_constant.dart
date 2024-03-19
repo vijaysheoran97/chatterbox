6f68dc11de877b15a1fa484131eeb14c22ec2d57
Vijay Sheoran <vijaysirsi1997@gmail.com>
2024-03-19 16:43:50 +0530

    - Login Screen - Forget Screen - SignUp Screen - Email Login Screen

--- lib/chatter_box/utils/app_string_constant.dart
+++ lib/chatter_box/utils/app_string_constant.dart
@@ -1,6 +1,20 @@
 class AppStringConstant {
 
   static const String appName = 'ChatterBox';
+  static String email ='Email';
+  static String password ='Password';
+  static String confirmPassword ='Confirm Password';
+  static String forgetPassword ='Forget Password?';
+  static String login1 ='LOGIN';
+  static String signUp =' Sign up';
+  static String signUp11 =' SIGNUP';
+  static String donotHaveAnAccount ="Don't have an account? ";
+  static String alreadyHaveAnAccount ="Already have an account? ";
+  static String login =' Login';
+  static String signUpWithGoogle = 'Sign in with google';
+
+
+
 
 
 }
