e4decfb9c29571f48ff147a86b2549c90d81748c
Parmila <parmilasheoran570@gmail.com>
2024-03-22 10:09:03 +0530

    - provider implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 
 import 'package:chatterbox/chatter_box/auth/email_login_screen.dart';
 import 'package:chatterbox/chatter_box/screens/homeScreen.dart';
+import 'package:chatterbox/chatter_box/service/auth_service.dart';
 import 'package:chatterbox/chatter_box/settings/settings.dart';
+import 'package:chatterbox/chatter_box/utils/storage_halper.dart';
 import 'package:chatterbox/firebase_options.dart';
+import 'package:chatterbox/provider/auth_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:chatterbox/chatter_box/auth/login_screen.dart';
@@ -10,13 +13,14 @@ import 'package:chatterbox/chatter_box/settings/settings.dart';
 import 'package:chatterbox/chatter_box/settings/themes.dart';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'chatter_box/theme/dark_theme.dart';
 import 'chatter_box/theme/theme_manager.dart';
 
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
@@ -37,6 +41,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Get.put(AuthService());
+    Get.put(StorageHalper());
+    UserProvider userProvider =UserProvider();
+    userProvider.loadLoginStatus();
     return Consumer<ThemeManager>(
       builder: (context, themeManager, child) {
         return MaterialApp(
