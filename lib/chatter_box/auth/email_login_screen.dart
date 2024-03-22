1623b2ecff068fc768f8d8912f8b8c8b61aa9388
Parmila <parmilasheoran570@gmail.com>
2024-03-22 10:06:52 +0530

    - container remove

--- lib/chatter_box/auth/email_login_screen.dart
+++ lib/chatter_box/auth/email_login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:chatterbox/chatter_box/auth/sign_up-screen.dart';
 import 'package:chatterbox/chatter_box/dialogbox/forget_password_dialog.dart';
 import 'package:chatterbox/chatter_box/screens/homeScreen.dart';
-import 'package:chatterbox/chatter_box/settings/settings.dart';
 import 'package:chatterbox/chatter_box/utils/app_color_constant.dart';
 import 'package:chatterbox/chatter_box/utils/app_string_constant.dart';
 import 'package:chatterbox/model/user_info_model.dart';
@@ -17,20 +16,21 @@ class EmailLoginScreen extends StatefulWidget {
 }
 
 class _EmailLoginScreenState extends State<EmailLoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  bool _isPasswordVisible = false;
   @override
   Widget build(BuildContext context) {
 
-    TextEditingController emailController = TextEditingController();
-    TextEditingController passwordController = TextEditingController();
-    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
-    bool _isPasswordVisible = false;
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: SingleChildScrollView(
+      body:
+      Padding(
+          padding: const EdgeInsets.all(16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
@@ -44,50 +44,42 @@ class _EmailLoginScreenState extends State<EmailLoginScreen> {
               ),
               const SizedBox(height: 80),
               // Email TextField
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                width: 400,
-                child: TextFormField(
-                  controller: emailController,
-                  cursorColor: AppColorConstant.appText2Color,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    hintText: AppStringConstant.email,
-                    prefixIcon: const Icon(Icons.email_outlined),
-                    contentPadding: const EdgeInsets.only(top: 2, left: 8),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
+              TextFormField(
+                controller: emailController,
+                cursorColor: AppColorConstant.appText2Color,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: AppStringConstant.email,
+                  prefixIcon: const Icon(Icons.email_outlined),
+                  contentPadding: const EdgeInsets.only(top: 2, left: 8),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
                 ),
               ),
               const SizedBox(height: 24),
               // Password TextField
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                width: 400,
-                child: TextFormField(
-                  controller: passwordController,
-                  cursorColor: AppColorConstant.appText2Color,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: !_isPasswordVisible,
-                  decoration: InputDecoration(
-                    hintText: AppStringConstant.password,
-                    prefixIcon: const Icon(Icons.lock_outline),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          _isPasswordVisible = !_isPasswordVisible;
-                        });
-                      },
+              TextFormField(
+                controller: passwordController,
+                cursorColor: AppColorConstant.appText2Color,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: !_isPasswordVisible,
+                decoration: InputDecoration(
+                  hintText: AppStringConstant.password,
+                  prefixIcon: const Icon(Icons.lock_outline),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
                     ),
-                    contentPadding: const EdgeInsets.only(top: 2, left: 8),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  ),
+                  contentPadding: const EdgeInsets.only(top: 2, left: 8),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
                 ),
               ),
@@ -105,7 +97,7 @@ class _EmailLoginScreenState extends State<EmailLoginScreen> {
                   children: [
                     Text(
                       AppStringConstant.forgetPassword,
-                      style: TextStyle(color: AppColorConstant.forgetpassword),
+                      style: const TextStyle(color: AppColorConstant.forgetpassword),
                     ),
                   ],
                 ),
@@ -159,7 +151,7 @@ class _EmailLoginScreenState extends State<EmailLoginScreen> {
                   GestureDetector(
                     onTap: () {
                       Navigator.push(context, MaterialPageRoute(builder: (context) {
-                        return const SignUpScreen();
+                        return  SignUpScreen();
                       }));
                     },
                     child: Text(
@@ -175,7 +167,7 @@ class _EmailLoginScreenState extends State<EmailLoginScreen> {
             ],
           ),
         ),
-      ),
+      
     );
   }
   // Future login() async {
