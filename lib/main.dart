d1517acbe67d738f1f0dc8f8f6d62143479faac3
Vijay Sheoran <vijaysirsi1997@gmail.com>
2024-04-08 15:54:28 +0530

    - minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,126 @@
+// import 'dart:developer';
+// import 'dart:io';
+// import 'package:chatterbox/screens/splash_screen.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:chatterbox/chatter_box/provider/auth_provider.dart';
+// import 'package:chatterbox/chatter_box/service/auth_service.dart';
+// import 'package:chatterbox/chatter_box/utils/storage_halper.dart';
+// import 'package:chatterbox/firebase_options.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_notification_channel/flutter_notification_channel.dart';
+// import 'package:flutter_notification_channel/notification_importance.dart';
+// import 'package:get/get.dart';
+// import 'package:provider/provider.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+// import 'chatter_box/theme/dark_theme.dart';
+// import 'package:flutter/foundation.dart' as foundation;
+// import 'chatter_box/theme/theme_manager.dart';
+//
+// late Size mq;
+//
+// class MyHttpOverrides extends HttpOverrides {
+//   @override
+//   HttpClient createHttpClient(SecurityContext? context) {
+//     return super.createHttpClient(context)
+//       ..badCertificateCallback =
+//           (X509Certificate cert, String host, int port) => true;
+//   }
+// }
+//
+// void main() async {
+//   if (foundation.kDebugMode) {
+//     debugPrint('release mode');
+//   } else {
+//     debugPrint('debug mode');
+//   }
+//
+//   WidgetsFlutterBinding.ensureInitialized();
+//   HttpOverrides.global = MyHttpOverrides();
+//   SystemChrome.setPreferredOrientations(
+//           [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+//       .then((value) {
+//     _initializeFirebase();
+//     runApp(const MyApp());
+//   });
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   SharedPreferences prefs = await SharedPreferences.getInstance();
+//   String? selectedTheme = prefs.getString('selectedTheme');
+//   var result = await FlutterNotificationChannel.registerNotificationChannel(
+//       description: 'For Showing Message Notification',
+//       id: 'chats',
+//       importance: NotificationImportance.IMPORTANCE_HIGH,
+//       name: 'Chats');
+//   log('\nNotification Channel Result: $result');
+//   runApp(
+//     ChangeNotifierProvider(
+//       create: (context) => ThemeManager(selectedTheme),
+//       child: const MyApp(),
+//     ),
+//   );
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     Get.put(AuthService());
+//     Get.put(StorageHalper());
+//     AuthProvider userProvider = AuthProvider();
+//     userProvider.loadLoginStatus();
+//     return MultiProvider(
+//       providers: [
+//         ChangeNotifierProvider(create: (context) {
+//           return userProvider;
+//         })
+//       ],
+//       child: Consumer<ThemeManager>(
+//         builder: (context, themeManager, child) {
+//           return MaterialApp(
+//             debugShowCheckedModeBanner: false,
+//             theme: themeManager.currentTheme,
+//             darkTheme: darkTheme,
+//             title: 'Q',
+//             home: const SplashScreen(),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+//
+//
+// _initializeFirebase() async {
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+// }
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import 'dart:developer';
 import 'dart:io';
 import 'package:chatterbox/screens/splash_screen.dart';
@@ -19,6 +142,7 @@ import 'chatter_box/theme/theme_manager.dart';
 
 late Size mq;
 
+
 class MyHttpOverrides extends HttpOverrides {
   @override
   HttpClient createHttpClient(SecurityContext? context) {
@@ -28,7 +152,7 @@ class MyHttpOverrides extends HttpOverrides {
   }
 }
 
-void main() async {
+void main()async {
   if (foundation.kDebugMode) {
     debugPrint('release mode');
   } else {
@@ -38,11 +162,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HttpOverrides.global = MyHttpOverrides();
   SystemChrome.setPreferredOrientations(
-          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
-      .then((value) {
-    _initializeFirebase();
-    runApp(const MyApp());
-  });
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -55,12 +175,13 @@ void main() async {
       importance: NotificationImportance.IMPORTANCE_HIGH,
       name: 'Chats');
   log('\nNotification Channel Result: $result');
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => ThemeManager(selectedTheme),
-      child: const MyApp(),
-    ),
-  );
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeManager(selectedTheme),
+    child: const MyApp(),
+  ),);
+  // }
+  // );
+
 }
 
 class MyApp extends StatelessWidget {
@@ -68,13 +189,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     Get.put(AuthService());
     Get.put(StorageHalper());
-    AuthProvider userProvider = AuthProvider();
+    AuthProvider userProvider =AuthProvider();
     userProvider.loadLoginStatus();
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) {
+        ChangeNotifierProvider(create: (context){
           return userProvider;
         })
       ],
@@ -84,20 +206,22 @@ class MyApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             theme: themeManager.currentTheme,
             darkTheme: darkTheme,
+
             title: 'Q',
-            home: const SplashScreen(),
+
+            home:  SplashScreen(),
+
+
           );
         },
       ),
+
     );
   }
 }
-
 
 _initializeFirebase() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 }
-
-
